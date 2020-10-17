e142a75011d848f355a83cffb9c8c50d14eac24b
proonestack <73004256+proonestack@users.noreply.github.com>
2020-10-17 07:13:09 +0700

    Update RFID.ino

--- RFID.ino
+++ RFID.ino
@@ -44,16 +44,14 @@ void loop()
      Serial.print("UID tag :");
      String content= "";
      byte letter;
-   
+   for (byte i = 0; i < mfrc522.uid.size; i++) 
   }
+  {
      Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
      Serial.print(mfrc522.uid.uidByte[i], HEX);
      content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
      content.concat(String(mfrc522.uid.uidByte[i], HEX));
-  }
-  Serial.println();
-  Serial.print("Message : ");
-  content.toUpperCase();
+
   if (content.substring(1) == "04 16 62 9A EF 2C 80" || content.substring(1) == "7D 8F 63 D3" || content.substring(1) == "04 5C 8C 2A 59 28 80") //Ganti UID TAG DENGAN PUNYA MAS-NYA
   {
     lcd.clear();
